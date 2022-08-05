e4b44bac7777e2f658f2b6933dc448cd8efa9f28
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-08-05 03:40:45 +0000

    terminado

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Template for Machine Learning projects
+# Times Series Project
+
+This time we will be using Times Series forecasting for anomaly detection on CPU usage, by a model using ARIMA (identify unexpected events in a process). The data of this project is based on per-minute metrics of the host’s CPU utilization.
+
+- cpu-train-a: https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv
+
+- cpu-train-b: https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-b.csv
+
+- cpu-test-a: https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv
+
+- cpu-test-b: https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-b.csv
--- src/app.py
+++ src/app.py
@@ -1,4 +1,48 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import pickle
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pmdarima.arima import auto_arima
 
-# your code here
+cpu_train_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv')
+cpu_train_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv')
+
+cpu_train_a['datetime'] = pd.to_datetime(cpu_train_a['datetime'])
+cpu_train_b['datetime'] = pd.to_datetime(cpu_train_b['datetime'])
+
+cpu_train_a = cpu_train_a.set_index('datetime')
+cpu_train_b = cpu_train_b.set_index('datetime')
+
+
+# model a
+stepwise_model_a = auto_arima(cpu_train_a, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+
+stepwise_model_a.fit(cpu_train_a)
+
+# model b
+stepwise_model_b = auto_arima(cpu_train_b, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+
+stepwise_model_b.fit(cpu_train_b)
+                           
+                        
+
+
+# Saving the models
+filename_a = '/workspace/Time-Series-Project/models/model_a.sav'
+pickle.dump(stepwise_model_a, open(filename_a, 'wb'))
+
+filename_b = '/workspace/Time-Series-Project/models/model_b.sav'
+pickle.dump(stepwise_model_b, open(filename_b, 'wb'))
